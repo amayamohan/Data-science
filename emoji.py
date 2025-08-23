53121aee19a50c4152dde62ccd2f9b63c04ab87d
amayamohan <amayamohan033@gmail.com>
2025-08-23 09:54:48 +0530

    emotion project

---
+++ emoji.py
@@ -0,0 +1,8 @@
+emotions = {"Happy": "😊", "Sad": "😢", "Angry": "😠", "Surprised": "😲"}
+print(emotions)
+
+
+ 
+emotion = input("Enter your message:").strip()
+print("Converted message:", emotions)
+
