78fd227115d108d015c9f8e872e68f74d2b8a21a
amayamohan <amayamohan033@gmail.com>
2025-08-23 10:04:06 +0530

    add this

--- emoji.py
+++ emoji.py
@@ -1,8 +1,15 @@
-emotions = {"Happy": "😊", "Sad": "😢", "Angry": "😠", "Surprised": "😲"}
+emotions={"happy": "😊", "sad": "😢", "angry": "😠", "love": "❤️", "surprised": "😲", "confused": "😕", "bored": "😐",
+         "excited": "🤩", "tired": "😴", "nervous": "😬"}
 print(emotions)
 
+# emotion = input("Enter an emotion: ").strip().lower()
+# if emotion in emotions:
+#     print(f"The emoji for {emotion} is {emotions[emotion]}")
+# else:
+#     print("Emotion not found.")
+text = input("Enter your message: ").strip().lower()
+words = text.split()
+converted = [emotions.get(word, word) for word in words]
+new_message = " ".join(converted)
 
- 
-emotion = input("Enter your message:").strip()
-print("Converted message:", emotions)
-
+print(new_message)
